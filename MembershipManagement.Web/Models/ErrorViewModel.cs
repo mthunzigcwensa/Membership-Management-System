43399aec95a4295f9f2df51a956814a59321096d
mthunzi gcwensa <mthunzigcwensa659@gmail.com>
2024-04-25 01:37:38 +0200

    Add project files.

---
+++ MembershipManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MembershipManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
